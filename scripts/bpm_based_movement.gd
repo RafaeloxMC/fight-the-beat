ca7a06853d724bec9efcb525e717e49c8315a23c
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-02-05 18:34:10 +0100

    Add smooth tile-based movement with auto deletion

--- scripts/bpm_based_movement.gd
+++ scripts/bpm_based_movement.gd
@@ -1,12 +1,22 @@
-extends Node3D
+extends Node
 
 var bpm: int = 130
+var tiles: Array[Node3D] = []
+var speed: float
 
 func _ready() -> void:
-	tick_bpm()
+	var notes = get_tree().get_nodes_in_group("TILE")
+	for note in notes:
+		if note is Node3D:
+			tiles.append(note)
+	print("Found " + str(tiles.size()) + " notes!")
+	
+	speed = bpm / 60.0
 
-func tick_bpm() -> void:
-	await get_tree().create_timer(60.0 / bpm).timeout
-	print("Ping")
-	self.position.z += 1
-	tick_bpm()
+func _process(delta: float) -> void:
+	for tile in tiles:
+		if !tile.is_queued_for_deletion() && tile.position.z > (tile.get_parent_node_3d() as CSGBox3D).size.z / 2:
+			tile.queue_free()
+			tiles.erase(tile)
+			print("Tile deleted!")
+		tile.position.z += speed * delta
