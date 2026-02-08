8553dcc91ae9e0c1370c5e04d9003b4adca93ec2
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-02-08 19:13:18 +0100

    Add tile spawning logic

--- scripts/bpm_based_movement.gd
+++ scripts/bpm_based_movement.gd
@@ -3,7 +3,7 @@ extends Node
 @onready var trigger_line: Area3D = $TriggerLine
 @onready var debug: Label = $"UI Container/DEBUG"
 
-@export var base_tile = PackedScene
+@export var base_tile: PackedScene
 @export var lanes: Array[Node3D] = []
 
 var bpm: int = 130
@@ -13,17 +13,18 @@ var speed: float
 var speed_multiplier = 2
 
 func _ready() -> void:
-	var notes = get_tree().get_nodes_in_group("TILE")
-	for note in notes:
-		if note is Node3D:
-			tiles.append(note)
-	print("Found " + str(tiles.size()) + " notes!")
+	spawn_lane(SongManager.currently_playing.tiles_ll, lanes[0])
+	spawn_lane(SongManager.currently_playing.tiles_ml, lanes[1])
+	spawn_lane(SongManager.currently_playing.tiles_mr, lanes[2])
+	spawn_lane(SongManager.currently_playing.tiles_rr, lanes[3])
 	
+	print("Found " + str(tiles.size()) + " notes!")
 	speed = bpm / 60.0 * speed_multiplier
+
 
 func _process(delta: float) -> void:
 	
-	debug.text = SongManager.currently_playing.title + " - " + SongManager.currently_playing.artist + "(" + str(SongManager.currently_playing.bpm) + " BPM)" + "\nLL: " + str(SongManager.currently_playing.tiles_ll) + "\nML: " + str(SongManager.currently_playing.tiles_ml) + "\nMR: " + str(SongManager.currently_playing.tiles_mr) + "\nRR: " + str(SongManager.currently_playing.tiles_rr)
+	debug.text = SongManager.currently_playing.title + " - " + SongManager.currently_playing.artist + " (" + str(SongManager.currently_playing.bpm) + " BPM)" + "\nLL: " + str(SongManager.currently_playing.tiles_ll) + "\nML: " + str(SongManager.currently_playing.tiles_ml) + "\nMR: " + str(SongManager.currently_playing.tiles_mr) + "\nRR: " + str(SongManager.currently_playing.tiles_rr)
 	
 	for tile in tiles:
 		if !tile.is_queued_for_deletion() && tile.position.z > (tile.get_parent_node_3d() as CSGBox3D).size.z / 2:
@@ -53,10 +54,17 @@ func _process(delta: float) -> void:
 				tiles.erase(tile)
 				print("Tile removed")
 
-func add_new_note_to_lane(lane: Node3D) -> void:
-	var new_tile = base_tile.new().instantiate()
+func add_new_note_to_lane(lane: Node3D, offset: float) -> void:
+	print("Offset: " + str(offset))
+	var new_tile = base_tile.instantiate()
 	if new_tile is not Node3D:
 		new_tile.queue_free()
 		return
+	new_tile.position.z -= offset * 2
 	lane.add_child(new_tile as Node3D)
 	tiles.append(new_tile as Node3D)
+
+func spawn_lane(pattern: Array[bool], lane: Node3D) -> void:
+	for i in pattern.size():
+		if pattern[i]:
+			add_new_note_to_lane(lane, i)
