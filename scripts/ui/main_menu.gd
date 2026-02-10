bb7baad5dd650430b297989fad4b7f79c5bbfa34
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-02-10 18:50:45 +0100

    Add main menu background

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,14 +1,58 @@
 extends Control
 
+@export var base_tile: PackedScene
+@export var lanes: Array[CSGBox3D] = []
+
+var bpm: int = 130
+var tiles: Array[CSGBox3D] = []
+var speed: float
+
+var speed_multiplier = 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	spawn_lane(lanes[0])
+	spawn_lane(lanes[1])
+	spawn_lane(lanes[2])
+	spawn_lane(lanes[3])
+	
+	speed = bpm / 60.0 * speed_multiplier
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	for tile in tiles:
+		if !tile.is_queued_for_deletion() && tile.position.z > (tile.get_parent_node_3d() as CSGBox3D).size.z / 2:
+			tile.queue_free()
+			tiles.erase(tile)
+			print("Tile deleted!")
+		tile.position.z += speed * delta
+		
+	
+	var lanes_queued_for_deletion: Array[String] = []
+	
+	if Input.is_action_just_pressed("LEFT_LANE"):
+		lanes_queued_for_deletion.append("LL")
+	if Input.is_action_just_pressed("ML_LANE"):
+		lanes_queued_for_deletion.append("ML")
+	if Input.is_action_just_pressed("MR_LANE"):
+		lanes_queued_for_deletion.append("MR")
+	if Input.is_action_just_pressed("RIGHT_LANE"):
+		lanes_queued_for_deletion.append("RR")
+
+func add_new_note_to_lane(lane: CSGBox3D) -> void:
+	var new_tile = base_tile.instantiate()
+	if new_tile is not CSGBox3D:
+		new_tile.queue_free()
+		return
+	new_tile.position.z -= (lane).size.z / 2
+	lane.add_child(new_tile)
+	tiles.append(new_tile)
+
+func spawn_lane(lane: Node3D) -> void:
+	print("Spawned!")
+	add_new_note_to_lane(lane)
+	await get_tree().create_timer(randf_range(0.5, 4)).timeout
+	spawn_lane(lane)
 
 
 func _on_play_pressed() -> void:
